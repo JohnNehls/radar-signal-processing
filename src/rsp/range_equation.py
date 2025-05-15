3552ada00772552ce6bf838fc7ba8f0e31ff5550
JohnNehls <nehls@email.arizona.edu>
2025-05-14 21:05:49 -0400

    improve range_equation doc strings

--- src/rsp/range_equation.py
+++ src/rsp/range_equation.py
@@ -2,7 +2,8 @@ from . import constants as c
 
 
 def signal_range_eqn(Pt, Gt, Gr, sigma, wavelength, R, L):
-    """Signal power in Watts considering transmit and recieve gain, RCS, wavelength, range, and losses
+    """
+    Signal power in Watts.
     Args:
         Pt (float) : Transmit power [W]
         Gt (float) : Transmit gain [unitless]
@@ -12,42 +13,87 @@ def signal_range_eqn(Pt, Gt, Gr, sigma, wavelength, R, L):
         R (float) : Range [m]
         L (float) : Loss [unitless]
     Return:
-        (float) signal power [S]
+        power : float [W]
     """
     return (Pt * Gt * Gr * sigma * wavelength**2) / (((4 * c.PI) ** 3) * (R**4) * L)
 
 
 def noise_power(B, F, T):
-    """Single-pulse SNR for uncoded pulse
+    """
+    Noise power in Watts.
     Args:
         B (float) : Bandwidth of the reciever [Hz]
         F (float) : Noise factor of the reciever [unitless]
         T (float) : Temperature of the system [Kelvin]
     Return:
-        Power of the noise
+        power : float [W]
     """
     return c.K_BOLTZ * T * B * F
 
 
 def snr_range_eqn_uncoded(Pt, Gt, Gr, sigma, wavelength, R, B, F, L, T):
-    """Single-pulse SNR for uncoded pulse
-    Gt, Gr, F, and L can be either unitless or dB, but all must be te same units"""
+    """
+    Single-pulse SNR for an uncoded pulse. Gt, Gr, F, and L can be either unitless or dB, but all must be te same units.
+    Args:
+        Pt (float) : Transmit power [W]
+        Gt (float) : Transmit gain [unitless]
+        Gr (float) : Recieve gain [unitless]
+        sigma (float) : Radar cross section [m^2]
+        wavelength (float) : Wavelength [Hz]
+        R (float) : Range [m]
+        B (float) : Bandwidth of the reciever [Hz]
+        F (float) : Noise factor of the reciever [unitless]    
+        L (float) : Loss [unitless]    
+        T (float) : Temperature of the system [Kelvin]
+    Return:
+        SNR : float [unitless]
+    """
     return (Pt * Gt * Gr * sigma * wavelength**2) / (
         ((4 * c.PI) ** 3) * (R**4) * c.K_BOLTZ * T * B * F * L
     )
 
 
 def snr_range_eqn(Pt, Gt, Gr, sigma, wavelength, R, B, F, L, T, time_bandwidth_prod):
-    """Single-pulse SNR
-    Gt, Gr, F, and L can be either unitless or dB, but all must be te same units"""
+    """
+    Single-pulse SNR for a general pulse. Gt, Gr, F, and L can be either unitless or dB, but all must be te same units.
+    Args:
+        Pt (float) : Transmit power [W]
+        Gt (float) : Transmit gain [unitless]
+        Gr (float) : Recieve gain [unitless]
+        sigma (float) : Radar cross section [m^2]
+        wavelength (float) : Wavelength [Hz]
+        R (float) : Range [m]
+        B (float) : Bandwidth of the reciever [Hz]
+        F (float) : Noise factor of the reciever [unitless]
+        L (float) : Loss [unitless]
+        T (float) : Temperature of the system [Kelvin]
+        time_bandwidth_prod (float) : time-bandwidth product [unitless]
+    Return:
+        SNR : float [unitless]
+    """
     return (
         snr_range_eqn_uncoded(Pt, Gt, Gr, sigma, wavelength, R, B, F, L, T) * time_bandwidth_prod
     )
 
 
 def snr_range_eqn_cp(Pt, Gt, Gr, sigma, wavelength, R, B, F, L, T, n_p, time_bandwidth_prod):
-    """ "SNR of range equation with coherent processing (CP)
-    Gt, Gr, F, and L can be either unitless or dB, but all must be te same units"""
+    """SNR after coherent processing of a number of pulses. Gt, Gr, F, and L can be either unitless or dB, but all must be te same units.
+    Args:
+        Pt (float) : Transmit power [W]
+        Gt (float) : Transmit gain [unitless]
+        Gr (float) : Recieve gain [unitless]
+        sigma (float) : Radar cross section [m^2]
+        wavelength (float) : Wavelength [Hz]
+        R (float) : Range [m]
+        B (float) : Bandwidth of the reciever [Hz]
+        F (float) : Noise factor of the reciever [unitless]
+        L (float) : Loss [unitless]
+        T (float) : Temperature of the system [Kelvin]
+        n_p (float) : number of pulses coherently processed [unitless]
+        time_bandwidth_prod (float) : time-bandwidth product [unitless]
+    Return:
+        SNR : float [unitless]
+    """
     singlePulse_snr = snr_range_eqn(
         Pt, Gt, Gr, sigma, wavelength, R, B, F, L, T, time_bandwidth_prod
     )
@@ -55,27 +101,70 @@ def snr_range_eqn_cp(Pt, Gt, Gr, sigma, wavelength, R, B, F, L, T, n_p, time_ban
 
 
 def snr_range_eqn_bpsk_cp(Pt, Gt, Gr, sigma, wavelength, R, B, F, L, T, n_p, n_c):
-    """ "SNR of range equation with coherent processing (CP)
-    \tn_p := number of pulses
-    \tn_c := number of chips
-        Gt, Gr, F, and L can be either unitless or dB, but all must be te same units
+    """
+    SNR after coherent processing a number of binary phase shift keying pulses. Gt, Gr, F, and L can be either unitless or dB, but all must be te same units.
+    Args:
+        Pt (float) : Transmit power [W]
+        Gt (float) : Transmit gain [unitless]
+        Gr (float) : Recieve gain [unitless]
+        sigma (float) : Radar cross section [m^2]
+        wavelength (float) : Wavelength [Hz]
+        R (float) : Range [m]
+        B (float) : Bandwidth of the reciever [Hz]
+        F (float) : Noise factor of the reciever [unitless]
+        L (float) : Loss [unitless]
+        T (float) : Temperature of the system [Kelvin]
+        n_p (float) : number of pulses coherently processed [unitless]
+        n_c (float) : number of binary chips [unitless]
+    Return:
+        SNR : float [unitless]
     """
     return snr_range_eqn_cp(Pt, Gt, Gr, sigma, wavelength, R, B, F, L, T, n_p, n_c)
 
 
 def snr_rangeEquation_dutyFactor_pulses(Pt, Gt, Gr, sigma, wavelength, R, F, L, T, Tcpi, tau_df):
-    """ "SNR of range equation with coherent processing in duty factor form
-    \tTcpi := Total time of coherent processing interval (CPI) in seconds
-    \ttau_df := duty factor, in [0,1]
-        Gt, Gr, F, and L can be either unitless or dB, but all must be te same units
+    """SNR of range equation with coherent processing in duty factor form
+
+    SNR after coherent processing a number of uncodedpulses in duty factor form. Gt, Gr, F, and L can be either unitless or dB, but all must be te same units.
+    Args:
+        Pt (float) : Transmit power [W]
+        Gt (float) : Transmit gain [unitless]
+        Gr (float) : Recieve gain [unitless]
+        sigma (float) : Radar cross section [m^2]
+        wavelength (float) : Wavelength [Hz]
+        R (float) : Range [m]
+        B (float) : Bandwidth of the reciever [Hz]
+        F (float) : Noise factor of the reciever [unitless]
+        L (float) : Loss [unitless]
+        T (float) : Temperature of the system [Kelvin]
+        Tcpi (float ): Total time of coherent processing interval (CPI) [s]
+        tau_df (float) : duty factor, in [0,1] [unitless]
+    Return:
+        SNR : float [unitless]
     """
+    assert tau_df <= 1 and tau_df >= 0, "duty factor must be in [0,1]."
+
     singlePulse_snr = snr_range_eqn_uncoded(Pt, Gt, Gr, sigma, wavelength, R, 1, F, L, T)
     return singlePulse_snr * Tcpi * tau_df
 
 
 def min_target_detection_range(Pt, Gt, Gr, sigma, wavelength, SNR_thresh, B, F, L, T):
-    """single pulse minimum detectable range for a SNR_threshold
-    Gt, Gr, F, and L can be either unitless or dB, but all must be te same units
+    """
+    Minimum detectable range for a single uncoded pulse. Gt, Gr, F, and L can be either unitless or dB, but all must be te same units.
+    Args:
+        Pt (float) : Transmit power [W]
+        Gt (float) : Transmit gain [unitless]
+        Gr (float) : Recieve gain [unitless]
+        sigma (float) : Radar cross section [m^2]
+        wavelength (float) : Wavelength [Hz]
+        SNR_thresh (float) : SNR threshold
+        R (float) : Range [m]
+        B (float) : Bandwidth of the reciever [Hz]
+        F (float) : Noise factor of the reciever [unitless]
+        L (float) : Loss [unitless]
+        T (float) : Temperature of the system [Kelvin]
+    Return:
+        range : float [m]
     """
     return (
         (Pt * Gt * Gr * sigma * wavelength**2)
@@ -86,10 +175,24 @@ def min_target_detection_range(Pt, Gt, Gr, sigma, wavelength, SNR_thresh, B, F, 
 def min_target_detection_range_bpsk_cp(
     Pt, Gt, Gr, sigma, wavelength, SNR_thresh, B, F, L, T, n_p, n_c
 ):
-    """Minimum detectable range for a SNR_threshold for BPSK pulses
-    \tn_p := number of pulses
-    \tn_c := number of chips
-        Gt, Gr, F, and L can be either unitless or dB, but all must be te same units
+    """
+    Minimum detectable range for coherntly processed BPSK pulses. Gt, Gr, F, and L can be either unitless or dB, but all must be te same units.
+    Args:
+        Pt (float) : Transmit power [W]
+        Gt (float) : Transmit gain [unitless]
+        Gr (float) : Recieve gain [unitless]
+        sigma (float) : Radar cross section [m^2]
+        wavelength (float) : Wavelength [Hz]
+        SNR_thresh (float) : SNR threshold
+        R (float) : Range [m]
+        B (float) : Bandwidth of the reciever [Hz]
+        F (float) : Noise factor of the reciever [unitless]
+        L (float) : Loss [unitless]
+        T (float) : Temperature of the system [Kelvin]
+        n_p (float) : number of pulses coherently processed [unitless]
+        n_c (float) : number of binary chips [unitless]
+    Return:
+        range : float [m]
     """
     onePulse = min_target_detection_range(Pt, Gt, Gr, sigma, wavelength, SNR_thresh, B, F, L, T)
     return onePulse * (n_p * n_c) ** (1 / 4)
@@ -98,10 +201,24 @@ def min_target_detection_range_bpsk_cp(
 def min_target_detection_range_dutyfactor_cp(
     Pt, Gt, Gr, sigma, wavelength, SNR_thresh, F, L, T, Tcpi, tau_df
 ):
-    """Minimum detectable range for a SNR_threshold for BPSK pulses
-    \tTcpi := Total time of coherent processing interval (CPI) in seconds
-    \tau_df := duty factor, in [0,1]
-        Gt, Gr, F, and L can be either unitless or dB, but all must be te same units
+    """
+    Minimum detectable range for coherntly processed uncoded pulses in duty factor form. Gt, Gr, F, and L can be either unitless or dB, but all must be te same units.
+    Args:
+        Pt (float) : Transmit power [W]
+        Gt (float) : Transmit gain [unitless]
+        Gr (float) : Recieve gain [unitless]
+        sigma (float) : Radar cross section [m^2]
+        wavelength (float) : Wavelength [Hz]
+        SNR_thresh (float) : SNR threshold
+        R (float) : Range [m]
+        B (float) : Bandwidth of the reciever [Hz]
+        F (float) : Noise factor of the reciever [unitless]
+        L (float) : Loss [unitless]
+        T (float) : Temperature of the system [Kelvin]
+        Tcpi (float ): Total time of coherent processing interval (CPI) [s]
+        tau_df (float) : duty factor, in [0,1] [unitless]
+    Return:
+        range : float [m]
     """
     onePulse = min_target_detection_range(Pt, Gt, Gr, sigma, wavelength, SNR_thresh, 1, F, L, T)
     return onePulse * (Tcpi * tau_df) ** (1 / 4)
