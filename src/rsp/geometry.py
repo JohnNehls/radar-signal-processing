051689668b6c7bb5f7f053fb59ab92e736bb87e1
JohnNehls <nehls@email.arizona.edu>
2025-05-20 19:59:17 -0400

    clean geometry doc strings

--- src/rsp/geometry.py
+++ src/rsp/geometry.py
@@ -3,7 +3,16 @@ from numpy.linalg import norm
 
 
 def range_and_rangerate(plat_pos: list, plat_vel: list, tgt_pos: list, tgt_vel: list):
-    """Calculate the range vector, range, and range-rate of a target relative to a platform"""
+    """
+    Calculate the range vector, range, and range-rate of a target relative to a platform.
+    Args:
+        plat_pos (list) : Platform position [x,y,z]
+        plat_vel (list) : Platform velocity [v_x,v_y,v_z]
+        tgt_pos (list) : target position [x,y,z]
+        tgt_vel (list) : target velocity [v_x,v_y,v_z]
+    Return:
+        Range_vector, Range, RangeRate_vector
+    """
     R_vec = np.array(
         [tgt_pos[0] - plat_pos[0], tgt_pos[1] - plat_pos[1], tgt_pos[2] - plat_pos[2]]
     )
