c3bc643541a0a84a3f42b564dd0ddaa15f4e7e9f
JohnNehls <nehls@email.arizona.edu>
2025-05-22 22:19:53 -0400

    clean up utilities

--- src/rsp/utilities.py
+++ src/rsp/utilities.py
@@ -2,24 +2,58 @@ import numpy as np
 from . import constants as c
 
 
-def power2db(power):
+def power2db(power: float):
+    """
+    Convert power in Watts to dB
+    Args:
+        power (float) : Power [W]
+    Return:
+        power (float) : Power [dB]
+    """
     return 10 * np.log10(power)
 
 
-def db2power(db):
+def db2power(db: float):
+    """
+    Convert power in dB to Watts
+    Args:
+        power (float) : Power [dB]
+    Return:
+        power (float) : Power [W]
+    """
     return 10 ** (db / 10)
 
 
-def volt2db(voltage):
+def volt2db(voltage: float):
+    """
+    Convert voltage in Volts in Amps to dB
+    Args:
+        voltage (float) : Voltage in [A]
+    Return:
+        voltage (float) : Voltage in [dB]
+    """
     return 20 * np.log10(voltage)
 
 
-def db2volt(db):
+def db2volt(db: float):
+    """
+    Convert voltage in dB to Volts
+    Args:
+        voltage (float) : Voltage in [dB]
+    Return:
+        voltage (float) : Voltage in [A]
+    """
     return 10 ** (db / 20)
 
 
 def phase_negpi_pospi(phase):
-    """return input phase in [-pi, pi)"""
+    """
+    Place the input phases into [-pi, pi).
+    Args:
+        phase (float) : Phase
+    Return:
+        phase (float) : Phase
+    """
 
     if not hasattr(phase, "__iter__"):
         phase = [phase]
@@ -34,13 +68,26 @@ def phase_negpi_pospi(phase):
 
 
 def phase_zero_twopi(phase):
-    """return input phase in [0, 2pi)"""
+    """
+    Place the input phases into [0, 2pi).
+    Args:
+        phase (float) : Phase
+    Return:
+        phase (float) : Phase
+    """
     phase = np.array(phase)
     phase = phase % (2 * c.PI)
     return phase
 
 
 def zero_to_smallest_float(array, value=1e-16):
-    """set all zero elements of input array to value"""
+    """
+    Set all zero elements of input array to value in place.
+    Args:
+        array : Input array
+        value : Value to set the array's zero values to
+    Return:
+        None
+    """
     indxs = np.where(array == 0)
     array[indxs] = value
